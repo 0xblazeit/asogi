18248f4d76163268a761da9a0a921bcea6295c47
leche <96605480+leche64@users.noreply.github.com>
2024-12-30 12:25:00 -0500

    d

--- app/page.js
+++ app/page.js
@@ -3,11 +3,10 @@ import RotatingObject from "./components/RotatingObject";
 export default function Home() {
   return (
     <main className="h-screen w-screen overflow-hidden">
-      {/* <RotatingObject walletAddress="0xcEB780385e065697669E33a07266303598D085fB" /> */}
-      {/* <RotatingObject walletAddress="0xEd46d30d321cDB2959DA0ad2dF5BC3c93AadE204" /> */}
       <RotatingObject walletAddress="0xcEB780385e065697669E33a07266303598D085fB" />
-      {/* <RotatingObject walletAddress="0x1234567890abcdef1234567890abcdef12345678" /> */}
-      {/* <RotatingObject walletAddress="0xABCDEF0123456789abcdef0123456789abcdef12" /> */}
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-3xl md:text-5xl font-bold text-white z-10 tracking-widest">
+        Hello World
+      </div>
     </main>
   );
 }
