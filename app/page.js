f1690a22ed9a6f8594af9b233daf6f21b8575595
leche <96605480+leche64@users.noreply.github.com>
2024-12-31 01:39:05 -0500

    d

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import RotatingObject from "./components/RotatingObject";
+import { TerminalTyping } from "./components/TerminalTyping";
 
 export default function Home() {
   return (
     <main className="h-screen w-screen overflow-hidden">
       <RotatingObject walletAddress="0xcEB780385e065697669E33a07266303598D085fB" />
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-3xl md:text-5xl font-bold text-white z-10 tracking-widest">
-        Hello World
+        <TerminalTyping />
       </div>
     </main>
   );
