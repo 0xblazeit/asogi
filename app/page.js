c347e2474903fa94c129b58826014326a8e3efba
leche <96605480+leche64@users.noreply.github.com>
2025-01-02 15:29:57 -0500

    clean up

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@
 
 import RotatingObject from "./components/RotatingObject";
 import { WelcomeText } from "./components/WelcomeText";
-import { RecordButton } from "./components/RecordButton";
 import { useRef } from "react";
 
 export default function Home() {
@@ -14,7 +13,6 @@ export default function Home() {
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-3xl md:text-5xl font-bold text-white z-10 tracking-widest">
         <WelcomeText />
       </div>
-      {/* <RecordButton canvasRef={canvasRef} /> */}
     </main>
   );
 }
