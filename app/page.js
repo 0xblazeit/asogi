3c2ae6d5d47dea682c8e34d696b79c38847e3e51
leche <96605480+leche64@users.noreply.github.com>
2025-01-01 21:32:10 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -10,14 +10,11 @@ export default function Home() {
 
   return (
     <main className="h-fit w-fit overflow-hidden">
-      <RotatingObject 
-        walletAddress="0xcEB780385e065697669E33a07266303598D085fB" 
-        canvasRef={canvasRef} 
-      />
+      <RotatingObject walletAddress="0xcEB780385e065697669E33a07266303598D085fB" canvasRef={canvasRef} />
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-3xl md:text-5xl font-bold text-white z-10 tracking-widest">
         <WelcomeText />
       </div>
-      <RecordButton canvasRef={canvasRef} />
+      {/* <RecordButton canvasRef={canvasRef} /> */}
     </main>
   );
 }
