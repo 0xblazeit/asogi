32c2f7a7094e2f80ca174c3d4beb66e014c9c61a
leche <96605480+leche64@users.noreply.github.com>
2025-01-02 21:23:24 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -14,9 +14,6 @@ export default function Home() {
       <RotatingObject walletAddress="0xcEB780385e065697669E33a07266303598D085fB" canvasRef={canvasRef} />
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-3xl md:text-5xl font-bold text-white z-10 tracking-widest">
         <WelcomeText onComplete={() => setShowMintButton(true)} />
-        <div className="flex justify-center mt-8">
-          <MintButton canvasRef={canvasRef} show={showMintButton} />
-        </div>
       </div>
     </main>
   );
