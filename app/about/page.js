108faf3dc45ac411d14cba28fb1f895ec83e27d6
leche <96605480+leche64@users.noreply.github.com>
2025-01-04 13:34:21 -0500

    d

--- app/about/page.js
+++ app/about/page.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 export default function AboutPage() {
   return (
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1.75 }}>
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-3xl md:text-5xl font-bold text-white z-10 tracking-widest">
         <h1 className="text-6xl font-bold text-white text-center">WEN</h1>
       </div>
