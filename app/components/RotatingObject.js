46551c57fe6c0fda5bb63d03dc89d0c434d506d6
leche <96605480+leche64@users.noreply.github.com>
2025-01-05 13:50:36 -0500

    Update RotatingObject.js

--- app/components/RotatingObject.js
+++ app/components/RotatingObject.js
@@ -98,7 +98,7 @@ export default function RotatingObject({
 
     // Burst animation constants
     const burstConfig = {
-      timeBetweenBursts: 4000,
+      timeBetweenBursts: 6000,
       burstChance: 0.6,
       maxProgress: 1.2,
       progressIncrement: 0.02,
